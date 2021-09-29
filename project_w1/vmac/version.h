ef9e3c9f14bddedf8f7cd496beb8eeab182d184b
rongjun.chen <rongjun.chen@amlogic.com>
2021-09-29 20:36:08 +0800

    wifi: v1.2.3_20210906-r driver:29eae25d21eccad37858fd98af654954b06790f0 [1/1]
    PD#SWPL-59385
    
    Problem:
    1.change the driver version
    
    Solution:
    change the driver version
    
    Verify:
    s905Y4
    
    Change-Id: I91965529581a89991b83c83e2a74364b2ed8e0bb

--- project_w1/vmac/version.h
+++ project_w1/vmac/version.h
@@ -1,1 +1,2 @@
-#define DRIVERVERSION    "v1.0.1_20210916-r driver:655ed6bae84b7560eb18bbbce91ba278c480e036"
+#define DRIVERVERSION    "v1.2.3_20210906-r driver:29eae25d21eccad37858fd98af654954b06790f0"
+#define PATCH "655ed6bae84b7560eb18bbbce91ba278c480e036"
