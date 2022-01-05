ed0df1c375d4bf7223f3a33b7dc3bce31f46f220
Xindong Xu <xindong.xu@amlogic.com>
2022-01-05 17:58:39 +0800

    wifi: fix compile error [2/2]
    PD#SWPL-68990
    
    Problem:
    compile error, variable defined but not used
    
    Solution:
    add CFLAGS -Wno-unused-but-set-variable
    just for clang r437112
    
    Verify:
    ohm
    
    Change-Id: I171e0c3eaa305e95bf67c015b370bc71b7692051
    Signed-off-by: Xindong Xu <xindong.xu@amlogic.com>

---
+++ project_w1/vmac/print_version.c
@@ -0,0 +1,6 @@
+#include "wifi_hal_com.h"
+
+void print_driver_version(void) {
+    printk("driver compile date: 2022-01-05 17:41:39,driver hash: 5267d669651d6a927f937b304cbc0e6402b8a54b\n");
+    printk("fw compile date: 2022-01-04 17:34:04,fw hash: ,fw size: 1071764\n");
+}
