31663ff51a6d040c7f8e011cc747c495c1980a22
chengshuo.wang <chengshuo.wang@amlogic.com>
2022-09-09 14:38:14 +0800

    wifi: fix gitignore error [1/1]
    PD#SWPL-94011
    
    Problem:
    1. print_version.c cause git to fail
    
    Solution:
    1. add to .gitignore
    
    Verify:
    ok
    
    Change-Id: Ica48880c6b25a202d295a7db1f8495ad336bc9a7
    Signed-off-by: chengshuo.wang <chengshuo.wang@amlogic.com>

--- project_w1/vmac/print_version.c
+++
@@ -1,6 +0,0 @@
-#include "wifi_hal_com.h"
-
-void print_driver_version(void) {
-    printk("driver compile date: 2022-08-24 09:52:02,driver hash: a1a876ec8abc2afaf054a2b3e97120aea96d8b7b\n");
-    printk("fw compile date: 2022-08-16 21:13:31,fw hash: ,fw size: 1071764\n");
-}
